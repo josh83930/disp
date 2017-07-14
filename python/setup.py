8e6d29aa69ef42d23bdce0edb7c6cfcf102fe5c3
Anthony LaTorre <tlatorre@uchicago.edu>
2017-07-14 16:50:02 -0400

    bump version

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='dispatch',
-      version='1.0',
+      version='1.0.1',
       description='Python bindings for receiving data from the SNO+ dispatcher',
       author="Anthony LaTorre",
       author_email="tlatorre@uchicago.edu",
